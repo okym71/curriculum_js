1b124e18b82899dc96320e1c613b1f6f25ea542f
okym71 <88600571+okym71@users.noreply.github.com>
2021-09-11 11:54:24 +0900

    JavaScript　1-3 課題

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,18 @@
+let a = 11;
+let b = 21;
+console.log(a !== 10);
+console.log(b >= 10 && b < 20);
+
+let x = 10;
+if(x >= 10 && x <= 20){
+  console.log('成功です');
+} else {
+  console.log('失敗です');
+}
+
+let y = 0;
+if(y % 2 == 0){
+  console.log('偶数です');
+} else {
+  console.log('奇数です');
+}
